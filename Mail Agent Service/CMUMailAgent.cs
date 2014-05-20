9104d0678123e569919832d71893b842ebccca76
Nikko Miu <miu2n@cmich.edu>
2014-05-20 15:40:36 -0400

    Moved Thread process to ServiceThread Class

--- Mail Agent Service/CMUMailAgent.cs
+++ Mail Agent Service/CMUMailAgent.cs
@@ -1,122 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.ServiceProcess;
-using System.Threading;
 
 namespace Mail_Agent_Service
 {
     public partial class CMUMailAgent : ServiceBase
     {
-        private Thread mainThread;
-        private Logging Log;
-        private Settings settings;
-
+        ServiceThread _svcThread;
+        
         public CMUMailAgent()
         {
-            
+            // Create new Service Thread object
+            _svcThread = new ServiceThread();
         }
 
         protected override void OnStart(string[] args)
         {
-            this.Start();
+            base.OnStart(args);
+
+            // Start the Service Thread
+            _svcThread.Start();
         }
 
         protected override void OnStop()
         {
             base.OnStop();
 
-            // Write the end statement to the log
-            Log.End();
-
-            // Destroy the thread
-            mainThread.Abort();
-        }
-
-        private void Start()
-        {
-            // Create a new thread for the thread loop
-            mainThread = new Thread(ThreadLoop);
-
-            // Start the thread loop
-            mainThread.Start();
-        }
-
-        private void ThreadLoop()
-        {
-#if DEBUG
-            // Make the thread sleep long enough to attach a debugger to the process
-            Thread.Sleep(10000);
-#endif
-            // Parse the settings file
-            this.settings = new Settings();
-            settings.Parse();
-
-            // Convert LogLocalLocation setting from string to bool
-            bool localLocation;
-            bool.TryParse(settings.General["LogLocalLocation"], out localLocation);
-
-            // Convert LogLevel from string to enum
-            Logging.Level logLevel;
-            Enum.TryParse(settings.General["LogLevel"], true, out logLevel);
-
-            // Setup the logging file
-            Log = new Logging(settings.General["LogLocation"], logLevel, localLocation);
-
-            // Log the start of the thread with some settings information
-            Log.Begin(settings.General);
-
-            // Convert MailPolling variable into an int
-            int threadSleep;
-            int.TryParse(settings.General["MailPolling"], out threadSleep);
-
-            // Loop "forever"
-            while (true)
-            {
-                try
-                {
-                    ExchangeServer exchange = new ExchangeServer(settings.General);
-
-                    exchange.SaveMail(settings.Profiles, Log);
-                }
-
-                // Catch exception for thread abort (OnStop)
-                catch (ThreadAbortException)
-                {
-                    Log.WriteLine(Logging.Level.INFO, "Thread exiting!");
-                }
-
-                // Catch exception for Exchange XML Error
-                catch (Microsoft.Exchange.WebServices.Data.ServiceXmlDeserializationException ex)
-                {
-                    // Output the more generic error to the logs
-                    Log.WriteError(ex);
-
-                    // Output the slightly more specific error message to logs
-                    Log.WriteLine(Logging.Level.CRITICAL, "Exchange Web Service Encountered an error!");
-                    Log.WriteLine(Logging.Level.CRITICAL, "  This could be caused by a login failure.");
-                    Log.WriteLine(Logging.Level.CRITICAL, "  Check your connection information in the settings and restart the service.");
-
-                    // Quit the service because this error cannot be recovered from
-                    return;
-                }
-
-                // Catch other general exceptions
-                catch (Exception ex)
-                {
-                    // Write the exception to the log
-                    Log.WriteError(ex);
-                }
-
-                // Sleep for the set time in the settings
-                Thread.Sleep(threadSleep);
-
-                // Garbage collection
-                Log.WriteLine(Logging.Level.DEBUG, "Memory Allocated (Before): " + GC.GetTotalMemory(false));
-                Log.WriteLine(Logging.Level.DEBUG, "Collecting Garbage...");
-                GC.Collect();
-                Log.WriteLine(Logging.Level.DEBUG, "Memory Allocated (After) : " + GC.GetTotalMemory(true));
-            }
+            // Stop the Service Thread Gracefully
+            _svcThread.Stop();
         }
     }
 }
---
+++ Mail Agent Service/ServiceThread.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mail_Agent_Service
+{
+    class ServiceThread
+    {
+        private Logging _log;
+        private Settings _settings;
+
+        private CancellationTokenSource _cancelToken;
+        private ManualResetEvent _resetEvent;
+
+        private readonly object _lockObject;
+
+        public ServiceThread()
+        {
+            _lockObject = new object();
+
+            // Parse the settings file
+            this._settings = new Settings();
+            _settings.Parse();
+
+            // Convert LogLocalLocation setting from string to bool
+            bool localLocation;
+            bool.TryParse(_settings.General["LogLocalLocation"], out localLocation);
+
+            // Convert LogLevel from string to enum
+            Logging.Level logLevel;
+            Enum.TryParse(_settings.General["LogLevel"], true, out logLevel);
+
+            // Setup the logging file
+            _log = new Logging(_settings.General["LogLocation"], logLevel, localLocation);
+
+            // Log the start of the thread with some settings information
+            _log.Begin(_settings.General);
+        }
+
+        public void Start()
+        {
+            // Create the cancel token object
+            _cancelToken = new CancellationTokenSource();
+
+            // Create the reset event handler
+            _resetEvent = new ManualResetEvent(false);
+
+            // Create and start the thread
+            Task.Factory.StartNew(() => ThreadLoop(), _cancelToken.Token);
+        }
+
+        public void Stop()
+        {
+            // Create and start new thread to kill process thread
+            new Thread(() =>
+            {
+                lock (_lockObject)
+                {
+                    Monitor.Pulse(_lockObject);
+                }
+
+                // Cancel the reset delay
+                _resetEvent.Set();
+
+                // Cancel the thread
+                _cancelToken.Cancel();
+
+                // Write the end statement to the log
+                _log.End();
+            }).Start();
+        }
+
+        private void ThreadLoop()
+        {
+#if DEBUG
+            // Make the thread sleep long enough to attach a debugger to the process
+            Thread.Sleep(10000);
+#endif
+
+            // Convert MailPolling variable into an int
+            int threadSleep;
+            int.TryParse(_settings.General["MailPolling"], out threadSleep);
+
+            // Loop "forever"
+            while (true)
+            {
+                _log.WriteLine(Logging.Level.DEBUG, "Reached Start of Thread Loop");
+
+                try
+                {
+                    // Create a new ExchangeServer
+                    ExchangeServer exchange = new ExchangeServer(_settings.General);
+
+                    // Save mail in Exchange
+                    exchange.SaveMail(_settings.Profiles, _log);
+                }
+
+                // Catch exception for thread abort (OnStop)
+                catch (ThreadAbortException)
+                {
+                    _log.WriteLine(Logging.Level.ERROR, "Thread aborting!");
+                }
+
+                // Catch exception for Exchange XML Error
+                catch (Microsoft.Exchange.WebServices.Data.ServiceXmlDeserializationException ex)
+                {
+                    // Output the more generic error to the logs
+                    _log.WriteError(ex);
+
+                    // Output the slightly more specific error message to logs
+                    _log.WriteLine(Logging.Level.CRITICAL, "Exchange Web Service Encountered an error!");
+                    _log.WriteLine(Logging.Level.CRITICAL, "  This could be caused by a login failure.");
+                    _log.WriteLine(Logging.Level.CRITICAL, "  Check your connection information in the settings and restart the service.");
+
+                    // Quit the service because this error cannot be recovered from
+                    return;
+                }
+
+                // Catch other general exceptions
+                catch (Exception ex)
+                {
+                    // Write the exception to the log
+                    _log.WriteError(ex);
+                }
+
+                // Collect the garbage
+                _log.WriteLine(Logging.Level.DEBUG, "Memory Allocated (Before): " + GC.GetTotalMemory(false));
+                _log.WriteLine(Logging.Level.DEBUG, "Collecting Garbage...");
+                GC.Collect();
+                _log.WriteLine(Logging.Level.DEBUG, "Memory Allocated (After) : " + GC.GetTotalMemory(true));
+
+                lock (_lockObject)
+                {
+                    // Sleep for the set time in the settings
+                    if (Monitor.Wait(_lockObject, new TimeSpan(0, 0, 0, 0, threadSleep)))
+                    {
+                        _log.WriteLine(Logging.Level.CRITICAL, "ABC");
+                    }
+                    else
+                    {
+                        _log.WriteLine(Logging.Level.CRITICAL, "ABC");
+                    }
+
+                    // If there was a request to cancel the thread
+                    if (_cancelToken.Token.IsCancellationRequested)
+                        // Exit the thread
+                        return;
+                }
+
+                _log.WriteLine(Logging.Level.DEBUG, "Reached End of Thread Loop");
+            }
+        }
+    }
+}
