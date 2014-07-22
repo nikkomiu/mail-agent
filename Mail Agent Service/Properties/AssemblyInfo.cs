ed42d3c0eecff43841708339206150555f1750d6
Nikko Miu <miu2n@cmich.edu>
2014-07-22 13:38:39 -0400

    Updated Assembly Info

--- Mail Agent Service/Properties/AssemblyInfo.cs
+++ Mail Agent Service/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Mail Agent")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Central Michigan University")]
 [assembly: AssemblyProduct("Mail Agent Service")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
