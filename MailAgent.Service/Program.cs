92eddb512acf5113b6e99c031919cf209667b6a6
Nikko Miu <miu2n@cmich.edu>
2014-07-22 15:51:39 -0400

    Created auto-installer for service

--- MailAgent.Service/Program.cs
+++ MailAgent.Service/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +14,51 @@ namespace MailAgent.Service
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-            { 
-                new CMUMailAgent() 
-            };
-            ServiceBase.Run(ServicesToRun);
+            if (System.Environment.UserInteractive)
+            {
+                if (args.Length > 0)
+                {
+                    switch (args[0].ToLower())
+                    {
+                        case "-u":
+                        case "-uninstall":
+                            ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+                            Console.WriteLine("Service Uninstalled!");
+                            break;
+                        case "-i":
+                        case "-install":
+                            ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+                            Console.WriteLine("Service Installed!");
+                            break;
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        Console.WriteLine("Service Removed!");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Service Not Installed!");                    
+                    }
+
+                    ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+                    Console.WriteLine("Service Installed!");
+                }
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+                { 
+                    new CMUMailAgent() 
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
         }
     }
 }
